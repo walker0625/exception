07abb34079353579c8481c42cec31f343981e0fd
전민우 <mw.jeon@lifesemantics.kr>
2022-04-29 14:08:47 +0900

    잘못된 import 교체

--- src/main/java/com/exception/WebServerCustomizer.java
+++ src/main/java/com/exception/WebServerCustomizer.java
@@ -1,16 +1,18 @@
 package com.exception;
 
-import org.springframework.boot.web.embedded.jetty.ConfigurableJettyWebServerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableJettyWebServerFactory> {
+@Slf4j
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
-    public void customize(ConfigurableJettyWebServerFactory factory) {
+    public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/400");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
 
--- src/main/java/com/exception/servlet/ErrorPageController.java
+++ src/main/java/com/exception/servlet/ErrorPageController.java
@@ -1,14 +1,20 @@
 package com.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
-//@Controller
+@Controller
 public class ErrorPageController {
 
     //RequestDispatcher 상수로 정의되어 있음
@@ -27,8 +33,23 @@ public class ErrorPageController {
 
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+        log.info("PAGE ERROR 500");
         printErrorInfo(request,response);
         return "error/500";
+    }
+
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String,Object>> errorJson500(HttpServletRequest request, HttpServletResponse response) {
+        log.info("API ERROR 500");
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
     }
 
     private void printErrorInfo(HttpServletRequest request, HttpServletResponse response) {
